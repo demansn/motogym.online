cc0428b733b0ad16518f7ee84be7d18422e48b7b
Dmytro Lysenko <demansn@gmail.com>
2022-07-13 14:34:03 +0300

    Fixed next.config.js

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -6,7 +6,8 @@ const nextConfig = {
   i18n,
   images: {
     domains: ['https://motogym-files.s3.eu-west-2.amazonaws.com', 'motogym-files.s3.eu-west-2.amazonaws.com']
-  }
+  },
+  distDir: 'build'
 };
 
 module.exports = nextConfig;
