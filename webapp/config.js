4dc05035a4f7046ed1fe58fda00ebee9aa0310e1
Dmytro Lysenko <demansn@gmail.com>
2023-05-05 16:26:06 +0300

    Added env varaibles

--- webapp/config.js
+++ webapp/config.js
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production';
 const DEV_URL = 'http://localhost:3000';
-const PROD_URL = 'https://motogym.online';
+const PROD_URL = process.env.NEXT_PUBLIC_API_URL;
 export const host = dev ? DEV_URL : PROD_URL;
 
