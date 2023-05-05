5e62546a7c1f44f1eb26a2cf258fe2ad71b1e261
Dmytro Lysenko <demansn@gmail.com>
2023-05-05 14:32:37 +0300

    Fixed 404 page

--- webapp/pages/404.js
+++ webapp/pages/404.js
@@ -1,34 +1,17 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {Container} from "react-bootstrap";
-import {useRouter} from "next/router";
 import {T} from "@components";
-import {useTranslation} from "next-i18next";
+import Link from "next/link";
 
  export default function Custom404() {
-     const { locale } = useRouter();
-     const {  i18n } = useTranslation();
-     // const {  re, setRe } = useState(false);
-     //
-     // useEffect(() => {
-     //     // if (i18n && i18n.addResourceBundle) {
-     //         i18n.addResourceBundle(locale);
-     //         i18n.changeLanguage(locale).then(() => setRe(true) );
-     //     // }
-     //
-     // },  []);
-
     return (
         <Container className="h-100">
             <div className="row justify-content-center h-75 align-items-center">
                 <div className="col-md-12 text-center">
                     <span className="display-1 d-block">404</span>
-                    <link href={'/'}><T>Home</T></link>
+                    <Link href={'/'}><T>Home</T></Link>
                 </div>
             </div>
         </Container>
     );
 }
-
-Custom404.layout = ({ children }) => {
-    return (<main>{children}</main>);
-};
