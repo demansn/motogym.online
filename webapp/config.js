412ab00fc72fef211ccff1623a4c2bec4a31ee43
Dmytro Lysenko <demansn@gmail.com>
2023-05-08 16:04:30 +0300

    Fixed config.js

--- webapp/config.js
+++ webapp/config.js
@@ -1,5 +1,3 @@
-const dev = process.env.NODE_ENV !== 'production';
-const DEV_URL = 'http://localhost:3000';
 const PROD_URL = process.env.NEXT_PUBLIC_API_URL;
-export const host = dev ? DEV_URL : PROD_URL;
+export const host = PROD_URL;
 
