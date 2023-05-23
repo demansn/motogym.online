799ff9cea0d374b048d50a53ed367d07ca013965
Dmytro Lysenko <demansn@gmail.com>
2023-05-23 13:56:27 +0300

    Add env parameters to dashboard

--- dashboard/src/configs.js
+++ dashboard/src/configs.js
@@ -1,4 +1,6 @@
 export const LocalesList = ['en', 'ua', 'ru', 'ja'];
-export const API_URI = 'http://localhost:3003/'
+export const API_URI = import.meta.env.VITE_PUBLIC_API_URL;
+
+console.log('API_URI', API_URI);
 export const GRAPHQL_API = `${API_URI}graphql`;
 export const AUTH_API = `${API_URI}auth`;
